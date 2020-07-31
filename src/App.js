94432731ec1f6a8e04a137cc8ab39c848ed91f4c
Paweł Patrzek <ppatrzek@bevy.pl>
2020-07-31 14:29:06 +0200

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <div class="container">
+        <div className="row">
+          <div className="col col-sm-12">
+            <button type="button" class="btn btn-primary">
+              Primary
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">One of three columns</div>
+        </div>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
